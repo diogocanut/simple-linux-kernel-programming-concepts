9af35255b180c61000aa05f7d210c6009ed2128e
diogocanut <diogocanut@hotmail.com>
2017-09-26 19:46:41 -0300

    ta indo

--- tasklets_workqueues_kthreads.c
+++ tasklets_workqueues_kthreads.c
@@ -17,28 +17,32 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Diogo Canut F. P.");
 
+/* VARIAVEL UTILIZADA PARA TRANSFORMAR JIFFIES EM SEGUNDOS */
 static unsigned long onesec;
 
 /* HANDLER */
 
 static void task_handler(unsigned long data)
 {
-
+  /* FUNÇÃO UTILIZADA PELO HANDLER */
   unsigned long j = jiffies;
   pr_info("---- task_handler %u jiffies\n", (unsigned)j);
 
 }
 
+/* DECLARANDO UMA TASKLET DE NOME TASKLET_HANDLER PARA EXECUTAR A FUNÇÃO
+TASK_HANDLER, RECEBENDO 0 NO CAMPO DATA */
 DECLARE_TASKLET(tasklet_handler, task_handler, 0);
 
 /* WORKQUEUE */
 
+/* PONTEIRO PARA A WORKQUEUE */
 struct workqueue_struct *wq = 0;
 
 
 static void work_handler(struct work_struct *w)
 {
-
+  /* FUNÇÃO UTILIZADA PELO WORK_HANDLER */
   unsigned long j = jiffies;
   pr_info("---- work_handler %u jiffies\n", (unsigned)j);
   msleep(500);
@@ -48,14 +52,15 @@ static void work_handler(struct work_struct *w)
 
 static void delayed_work_handler(struct work_struct *w)
 {
-
+  /* FUNÇÃO UTILIZADA PELO DELAYED WORK HANDLER */
   unsigned long j = jiffies;
   pr_info("---- delayed_work_handler %u jiffies\n", (unsigned)j);
   msleep(500);
 
 }
 
-
+/* DECLARANDO O WORKEQUEUE HANDLER E O DELAYED WORKQUEUE HANDLER PARAR EXECUTAR
+SUAS RESPECTIVAS FUNÇÕES */
 static DECLARE_WORK(workqueue_handler, work_handler); 
 static DECLARE_DELAYED_WORK(delayed_workqueue_handler, delayed_work_handler);
 
@@ -65,23 +70,25 @@ static DECLARE_DELAYED_WORK(delayed_workqueue_handler, delayed_work_handler);
 
 static void t_handler(unsigned long data)
 {
-
+  /* FUNÇÃO A SER EXECUTADA PELO KERNEL TIMER */
   unsigned long j = jiffies;
   pr_info("---- timer_handler %u jiffies\n", (unsigned)j);
 
 }
 
-
+/* DEFININDO O TIMER_HANDLER PARA EXECUTAR A FUNÇÃO T_HANDLER */
 DEFINE_TIMER(timer_handler, t_handler, 0, 0);
 
 
 /* KTHREAD */
 
+/* PONTEIROS PARA AS KTHREADS */
 static struct task_struct *td, *td_cpu;
 
 
 static int thread_handler(void *data)
 {
+  /* FUNÇÃO EXECUTADA PELA KTHREAD */
   unsigned long j = jiffies;
   pr_info("---- thread_handler %u jiffies\n", (unsigned)j);
   ssleep(5);
@@ -92,6 +99,7 @@ static int thread_handler(void *data)
 
 static int thread_cpu_handler(void *data)
 {
+  /* FUNÇÃO EXECUTADA PELA KTHREAD NA CPU 1 */
   unsigned long j = jiffies;
   pr_info("---- thread_cpu_handler %u jiffies\n", (unsigned)j);
   ssleep(5);
@@ -106,6 +114,7 @@ static int thread_cpu_handler(void *data)
 static int __init tasklets_workqueues_kthreads_init(void)
 {
   unsigned long j = jiffies;
+  /* TRANSFORMANDO JIFFIES EM SEGUNDOS */
   onesec = msecs_to_jiffies(1000 * 1);
 
   pr_info("----Inicio da função init -- %u \n", (unsigned)j);
@@ -150,7 +159,8 @@ static int __init tasklets_workqueues_kthreads_init(void)
 
   /* kthread_bind  !! */
 
-  
+  /* EXECUTANDO KTHREAD NA CPU 1, É UTILIZADA A FUNÇÃO CPU_TO_NODE PARA ADEQUAR AO PARAMETRO PEDIDO 
+  PELO FUNÇÃO KTHREAD */
   td_cpu = kthread_create_on_node(thread_cpu_handler, 0, cpu_to_node(1), "cpu_thread_handler");
   if(td_cpu)
   {
